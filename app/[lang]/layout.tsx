70ed92e48f9028cb3fb9bb933aacbcc25b73e526
yongjin <hyj919685260@gmail.com>
2025-01-31 05:46:47 +0800

    Add blog

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,6 +3,7 @@ import ThemeToggle from '@/components/ThemeToggle';
 import LanguageSelector from '@/components/LanguageSelector';
 import { Locale } from '../i18n/settings';
 import { Footer } from '@/components/Footer';
+import Link from 'next/link';
 interface Props {
   children: React.ReactNode;
   params: Promise<{ lang: Locale }>;
@@ -15,6 +16,15 @@ export default function LangLayout({ children, params }: Props) {
         <nav className="fixed w-full bg-white/80 dark:bg-gray-900/80 backdrop-blur-sm border-b border-gray-200 dark:border-gray-700  z-50">
           <div className="container mx-auto px-4 py-4 flex justify-between items-center">
             <LanguageSelector />
+            <Link
+              href="https://yongjinhuang.github.io/blog"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="text-gray-800 dark:text-gray-100 hover:text-blue-500 focus:outline-none transition-colors dark:hover:text-blue-500">
+                blog
+              </button>
+            </Link>
             <ThemeToggle />
           </div>
         </nav>
